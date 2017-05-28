427549554336397c450c5bb8d9277373bd8f8e7a
luislocon <luis.locon@gmail.com>
2017-05-28 17:12:55 -0600

    Agregando Javascript en el cliente, agregando dependecias Babel y Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var rename = require('gulp-rename')
+var rename = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream')
+var preset = require('babel-preset-es2015');
 
 gulp.task('styles', function(){
   gulp
@@ -8,13 +12,23 @@ gulp.task('styles', function(){
     .pipe(sass())
     .pipe(rename('style.css'))
     .pipe(gulp.dest('public'));
-})
+});
 //tarea de pasar los assets a archivos publicos
 gulp.task('assets', function(){
   gulp
     .src('assets/*')
     .pipe(gulp.dest('public'))
     //glob
-})
+});
 
-gulp.task('default', ['styles', 'assets'])
+//Transpila nuestro codigo para que sea compatible con todos los navegadores
+gulp.task('scripts', function(){
+  browserify('./src/index.js')
+    .transform(babel, preset)
+    .bundle()
+    .pipe(source('index.js'))
+    .pipe(rename('app.js'))
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('default', ['styles', 'assets', 'scripts'])
